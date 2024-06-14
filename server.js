1b999eed9f2479194292538f857d0f775a838c9b
Ted Perez <taperez1989@yahoo.com>
2024-06-13 22:51:01 -0700

    set up boiler plate still need to finish the api routes

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+// const api = require('./Develop/public/assets/js/index.js');
+const { v4 : uuidv4 } = require ('uuid');
+const app = express();
+const PORT = process.env.PORT || 3001
+
+const userId = uuidv4();
+console.log(userId);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+app.listen(PORT, () =>
+    console.log('app listening'));
+
+app.get('/api/notes', (req, res) => {
+    const savedNotes =JSON.parse( fs.readFileSync(path.join(__dirname, './db/db.json')))
+    console.log(savedNotes);
+    res.json(savedNotes)})
