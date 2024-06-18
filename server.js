9a63531c9b3c1c7e540df56c197395cc60004e41
Ted Perez <taperez1989@yahoo.com>
2024-06-17 19:40:02 -0700

    fixed my post request code, currently working on the delete request

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 // const api = require('./Develop/public/assets/js/index.js');
-const { v4 : uuidv4 } = require ('uuid');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3001
 
@@ -13,67 +13,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/api/notes', (req, res) => {
-    const savedNotes =JSON.parse( fs.readFileSync(path.join(__dirname, './db/db.json')))
+    console.log('get request for notes')
+    const savedNotes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')))
     console.log(savedNotes);
     res.json(savedNotes)
 })
-    
+
 app.post('/api/notes', (req, res) => {
-    // Log that a POST request was received
-    console.info(`${req.method} request received to add a note`);
-
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
-
-    // If all the required properties are present
-    if (title && text) {
-        // Variable for the object we will save
-        const newNote = {
-            title,
-            text,
-            userId: uuidv4(),
-        };
-
-//         fs.readFile('./db/db.json', 'utf8', (readErr, data) => {
-//             if (readErr) {
-//                 console.error(readErr);
-//                 res.status(500).send('Error reading notes file.');
-//             } else {
-//                 let notes = JSON.parse(data);
-//                 notes.push(newSavedNote);
-//                 fs.writeFile('./db/db.json', JSON.stringify(notes, null, 4), (writeErr) => {
-//                     if (writeErr) {
-//                         console.error(writeErr);
-//                         res.status(500).send('Error saving note.');
-//                     } else {
-//                         console.info('Successfully updated saved notes');
-//                         res.json(newSavedNote);
-//                     }
-//                 });
-//             }
-//         });
-//     } else {
-//         res.status(400).send('Title and text are required for a note.');
-//     }
-// })
-
-        const response = {
-            status: 'success',
-            body: newNote,
-        };
-
-        console.log(response);
-        res.status(201).json(response);
-    } else {
-        res.status(500).json('Error in posting review');
-    }
-});
+    console.log('post request for notes')
+    const savedNotes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json'),'utf-8'))
+    req.body.id = uuidv4();
+    savedNotes.push(req.body);
+    const newNote = fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
+    res.json(newNote);
+})
 
 app.delete('/api/notes/:id', (req, res) => {
-    const userId = req.params.id;
-
+    const savedNotes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf-8'))
+    const noteId = req.params.id;
+    const deletedNote = savedNotes.filter
+    const updatedNote = fs.writeFileSync('./db/db.json', JSON.stringify(updatedNote));
     console.log(userId);
-    res.send(`delete  request recieved for ID ${userId}`);
+    res.json(updatedNote);
 })
 
 
